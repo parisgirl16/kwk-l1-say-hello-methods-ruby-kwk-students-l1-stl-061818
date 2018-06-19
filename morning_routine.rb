a5612877241bf174b81a34c0194df8b4de827f1a
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-19 15:19:46 +0000

    Done.

---
+++ morning_routine.rb
@@ -0,0 +1,9 @@
+def morning_routine
+  puts "wake up"
+  puts "go to the bathroom"
+  puts "go downstairs to have breakfast"
+  puts "go back upstairs to brush teeth"
+  puts "get dressed"
+end
+morning_routine
+morning_routine

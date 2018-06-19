1dd8e571564a2a65490f4217b64674dac9e15045
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-19 14:46:44 +0000

    automatically backed up by learn

---
+++ morning_routine.rb
@@ -0,0 +1,8 @@
+def morning_routine
+  puts "wake up"
+  puts "go to the bathroom"
+  puts "go downstairs to have breakfast"
+  puts "go back upstairs to brush teeth"
+  puts "get dressed"
+end
+morning_routine

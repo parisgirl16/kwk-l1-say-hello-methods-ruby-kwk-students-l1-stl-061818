04792c892ebe732fa8ea5149aafba3562013b511
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-19 15:09:23 +0000

    automatically backed up by learn

--- argument.rb
+++ argument.rb
@@ -1,4 +1,4 @@
 def greeting(local_greeting="Hello",your_name,my_name,time_of_day)
   puts "#{local_greeting} #{your_name}, I am #{my_name} and the time is #{time_of_day}."
 end
-greeting("Caroline","Bella","10am")
+greeting("Howdy","Caroline","Bella","10am")
